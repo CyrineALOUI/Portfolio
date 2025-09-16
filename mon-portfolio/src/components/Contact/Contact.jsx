8a05094571595295e0cff4461b695ef0b2417b47
CyrineALOUI <cyrine.aloui@esprit.tn>
2025-09-16 17:25:22 +0100

    EmailJS Configuration

--- mon-portfolio/src/components/Contact/Contact.jsx
+++ mon-portfolio/src/components/Contact/Contact.jsx
@@ -1,25 +1,52 @@
 import "./Contact.css";
 import ContactImage from "../../assets/contact-image.png";
 import { FaPaperPlane } from "react-icons/fa";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
 export const Contact = () => {
-  return (
-    <section id="contact" className="contact">
-      <h2 className="contact-title">Contact Me</h2>
+    const form = useRef();
+    const serviceID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-      <div className="contact-container">
-        <div className="contact-illustration">
-          <img src={ContactImage} alt="Contact Illustration" />
-        </div>
 
-        <form className="contact-form">
-          <input type="text" placeholder="Name" required />
-          <input type="email" placeholder="Email" required />
-          <input type="text" placeholder="Object" required />
-          <textarea placeholder="Your Message" rows="5" required></textarea>
-          <button type="submit"> <FaPaperPlane /> Send Message</button>
-        </form>
-      </div>
-    </section>
-  );
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                serviceID,     //Service ID
+                templateID,    //Template ID
+                form.current,
+                publicKey     //Public Key
+            )
+            .then(
+                () => {
+                    alert("Message envoyé avec succès !");
+                },
+                (error) => {
+                    alert("Erreur : " + error.text);
+                }
+            );
+    };
+
+    return (
+        <section id="contact" className="contact">
+            <h2 className="contact-title">Contact Me</h2>
+
+            <div className="contact-container">
+                <div className="contact-illustration">
+                    <img src={ContactImage} alt="Contact Illustration" />
+                </div>
+
+                <form ref={form} onSubmit={sendEmail} className="contact-form">
+                    <input type="text" name="name" placeholder="Name" required />
+                    <input type="email" name="email" placeholder="Email" required />
+                    <textarea name="message" placeholder="Your Message" rows="5" required></textarea>
+                    <button type="submit"> <FaPaperPlane /> Send Message</button>
+                </form>
+            </div>
+        </section>
+    );
 };
